e82ca527afb428d997cac9e34fd7ed5b4a126ab9
KimJuHyeon <jennykim6065@naver.com>
2023-02-23 13:30:55 +0900

    회원가입 기본 UI

--- friendship/android/app/src/main/kotlin/com/example/friendship/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.friendship
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- friendship/android/app/src/main/kotlin/com/example/frienship/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.frienship
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
